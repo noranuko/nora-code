1e8fbc89aae684b8b9678cca1f688fbcad1e8e5e
Daisuke Watanabe <>
2024-09-02 08:28:37 +0900

    インデントをタブに変更

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,31 +1,31 @@
 module.exports = {
-  // astroのプラグインを使用（これがないと自動フォーマットしてくれない）
-  plugins: ['prettier-plugin-astro'],
-  // astroのパーサーを手動で指定（プラグイン公式推奨）
-  overrides: [
-    {
-      files: ['*.astro', '**/*.astro'],
-      options: {
-        parser: 'astro',
-      },
-    },
-  ],
-  // 1行の最大文字数
-  printWidth: 120,
-  // タブの幅
-  tabWidth: 2,
-  // タブではなくスペースを使用する
-  useTabs: false,
-  // ステートメントの末尾にセミコロンを出力する
-  semi: true,
-  // 文字列はシングルクォーテーションを利用する
-  singleQuote: true,
-  // 末尾にカンマを入れる
-  trailingComma: 'all',
-  // 閉じカッコを同一行に含めるか
-  bracketSameLine: true,
-  // アロー関数の引数の括弧を省略
-  arrowParens: 'avoid',
-  // タグ/周りの空白 (またはその欠如) は重要ではないと見なす
-  htmlWhitespaceSensitivity: 'ignore',
-}
+	// astroのプラグインを使用（これがないと自動フォーマットしてくれない）
+	plugins: ['prettier-plugin-astro'],
+	// astroのパーサーを手動で指定（プラグイン公式推奨）
+	overrides: [
+		{
+			files: ['*.astro', '**/*.astro'],
+			options: {
+				parser: 'astro',
+			},
+		},
+	],
+	// 1行の最大文字数
+	printWidth: 120,
+	// タブの幅
+	tabWidth: 2,
+	// タブではなくスペースを使用する
+	useTabs: true,
+	// ステートメントの末尾にセミコロンを出力する
+	semi: true,
+	// 文字列はシングルクォーテーションを利用する
+	singleQuote: true,
+	// 末尾にカンマを入れる
+	trailingComma: 'all',
+	// 閉じカッコを同一行に含めるか
+	bracketSameLine: true,
+	// アロー関数の引数の括弧を省略
+	arrowParens: 'avoid',
+	// タグ/周りの空白 (またはその欠如) は重要ではないと見なす
+	htmlWhitespaceSensitivity: 'ignore',
+};
--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -1,9 +1,9 @@
 module.exports = {
-  extends: ['stylelint-config-standard-scss', 'stylelint-config-recess-order'],
-  overrides: [
-    {
-      files: ['*.astro', '**/*.astro'],
-      customSyntax: 'postcss-html',
-    },
-  ],
+	extends: ['stylelint-config-standard-scss', 'stylelint-config-recess-order'],
+	overrides: [
+		{
+			files: ['*.astro', '**/*.astro'],
+			customSyntax: 'postcss-html',
+		},
+	],
 };
